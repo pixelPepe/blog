7ca99455fec95d3d14c6d5ae1aa70f3a7539cecc
GhUserrrr <101joe.prime@gmail.com>
2023-05-20 12:35:23 +0300

    add index tsx

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.scss';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Home } from 'pages';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+]);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
