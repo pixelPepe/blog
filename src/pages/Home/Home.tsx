f4463ae0c596c9e00e41e31c02054eb1c4902099
GhUserrrr <101joe.prime@gmail.com>
2023-05-21 16:11:48 +0300

    put posts inside container

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { HomePropsI } from './HomeProps';
-import styles from './Home.module.scss';
-import { Header } from 'components';
+import { Header, PostsList } from 'components';
+import { Container } from 'react-bootstrap';
 
 export const Home: React.FC<HomePropsI> = () => {
   return (
     <div>
       <Header />
+      <Container fluid>
+        <PostsList />
+      </Container>
     </div>
   );
 };
