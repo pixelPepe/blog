404eb0e8daf8a726698468d8e39f9813930f63c0
GhUserrrr <101joe.prime@gmail.com>
2023-05-21 16:07:00 +0300

    add burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,20 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { HeaderPropsI } from './HeaderProps';
 import styles from './Header.module.scss';
-import { Navbar, Container, Nav } from 'react-bootstrap';
+import { Navbar, Container, Offcanvas, ListGroup } from 'react-bootstrap';
+import cn from 'classnames';
+import { Link } from 'react-router-dom';
 
 export const Header: React.FC<HeaderPropsI> = () => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
   return (
     <Navbar bg="dark" variant="dark" expand="lg">
       <Container>
-        <Navbar.Brand href="#home">Logo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About me</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
+        <Link to={'/'}>
+          <Navbar.Brand>Logo</Navbar.Brand>
+        </Link>
+        <button className={styles.burgerBtn} onClick={handleShow}>
+          <span className={cn(show ? styles.active : null)}></span>
+        </button>
+        <Offcanvas show={show} onHide={handleClose}>
+          <Offcanvas.Header>
+            <Offcanvas.Title>Меню</Offcanvas.Title>
+          </Offcanvas.Header>
+          <Offcanvas.Body>
+            <ListGroup variant="flush">
+              <Link to={'/about'}>
+                <ListGroup.Item>Обо мне</ListGroup.Item>
+              </Link>
+              <Link to={'/'}>
+                <ListGroup.Item>Список постов</ListGroup.Item>
+              </Link>
+            </ListGroup>
+          </Offcanvas.Body>
+        </Offcanvas>
       </Container>
     </Navbar>
   );
